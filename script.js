617de1df7c8ff001bf1a1f8cc7797d72d2828d7b
Sundarapandiyan <sundarblue@gmail.com>
2026-02-06 11:43:51 +0530

    Updated website

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.querySelector(".cta-btn");
-  btn.addEventListener("click", () => {
-    alert("Welcome to MyPhotonics! This section can link to Products or About page.");
-  });
-});
-
