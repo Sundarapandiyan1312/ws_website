9b455c5f71cca9b7b645f25297c39ea485a19274
Your Name <sundarblue@gmail.com>
2026-02-19 12:52:03 +0530

    Update thankyou page

--- sendmail.php
+++ sendmail.php
@@ -1,31 +1,31 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-  $name    = $_POST['name'];
-  $email   = $_POST['email'];
-  $phone   = $_POST['phone'];
-  $company = $_POST['company'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
+$name    = $_POST['name'];
+$email   = $_POST['email'];
+$phone   = $_POST['phone'];
+$company = $_POST['company'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-  $to = "info@wsysind.com";   // company mail
+$to = "info@wsysind.com";
+$mail_subject = "New Website Contact Form Message";
 
-  $mail_subject = "New Website Contact Form Message";
+$body  = "You received a new message from website:\n\n";
+$body .= "Name: $name\n";
+$body .= "Email: $email\n";
+$body .= "Phone: $phone\n";
+$body .= "Company: $company\n";
+$body .= "Subject: $subject\n";
+$body .= "Message:\n$message\n";
 
-  $body  = "You received a new message from website:\n\n";
-  $body .= "Name: $name\n";
-  $body .= "Email: $email\n";
-  $body .= "Phone: $phone\n";
-  $body .= "Company: $company\n";
-  $body .= "Subject: $subject\n";
-  $body .= "Message:\n$message\n";
+$headers  = "From: Western Systems Website <info@wsysind.com>\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "Content-Type: text/plain; charset=utf-8\r\n";
 
-  $headers  = "From: info@wsysind.com\r\n";
-  $headers .= "Reply-To: $email\r\n";
+mail($to, $mail_subject, $body, $headers);
 
-  mail($to, $mail_subject, $body, $headers);
-
-  header("Location: thankyou.html");
-  exit();
+header("Location: thankyou.html");
+exit();
 }
 ?>
